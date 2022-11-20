56a3d696cec0087ad83b588b3fd98ff3965e474f
sholatel <yusuforiade1616@gmail.com>
2022-11-20 19:53:55 +0100

    Implemented dashboard layout

---
+++ src/components/Dashboard/Navigation.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@chakra-ui/react"
+
+export const Navigation = () => {
+    return (
+        <Box bg='white'
+            py={['32px','32px', '58px','58px']}
+            px={['10px', '10px','26px','26px']}
+            pos='sticky'
+            top='0px'
+        >
+            Navigation item
+        </Box>
+    )
+}
---
+++ src/components/Dashboard/ShoppingList.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/layout"
+
+export const ShoppingList  = ()=>  {
+    return  (
+        <Box bg='brand.orange'
+            py={['25px', '25px' ,'25px', '58px']}
+            px={['16px', '16px' ,'16px', '48px']}
+        >
+            Shopping List
+        </Box>
+    )
+}
